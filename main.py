dfee5ff6d06c38306041dcffef60401e402263ea
Dennis <denchikdron228@gmail.com>
2023-05-12 14:58:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+from tkinter import *
+import pandas
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
+
+# Read the data from the "words_to_learn.csv" file, if it exists, else read it from "french_words.csv" file
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+# Define function to display the next flashcard
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+# Define function to flip the flashcard and show the English translation
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+# Define function to remove the current card from the "to_learn" list and display the next card
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+# Create the GUI window
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+# Set a timer to flip the flashcard automatically after 3 seconds
+flip_timer = window.after(3000, func=flip_card)
+
+# Create the flashcard display area using a canvas widget
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+# Create buttons for "unknown" and "known" flashcards
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+# Start the application by displaying the first flashcard
+next_card()
+
+# Start the event loop for the GUI window
+window.mainloop()
